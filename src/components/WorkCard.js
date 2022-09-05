76074817c7b3f479e0761c11409a5ad551222f5d
dhruza88 <davidhruza@gmail.com>
2022-09-05 10:59:00 -0700

    styling touches

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -6,27 +6,23 @@ import React from 'react'
 
 
 const workCard = (props) => {
-  return (
-    <div className="project-card">
-    <img src={props.imgsrc} alt="image" />
-    <h2 className="project-title">{props.title}</h2>
-    <div className="pro-details">
-        <p>{props.text}</p>
-        <div className="pro-btns">
-            {/* <NavLink to={props.view}
-            className="btn">
-                View
-            </NavLink>
-            <NavLink to={props.source}
-            className="btn">
-                Source
-            </NavLink> */}
-            < a className="btn" target="_blank" href={props.view}>View</a>
-            < a className="btn" target="_blank" href={props.source}>Source</a>
+    return (
+        <div className="project-card">
+            <div>
+                <img src={props.imgsrc} alt="image" />
+            </div>
+            <div>
+                <h2 className="project-title">{props.title}</h2>
+            </div>
+            <div className="pro-details">
+                <p>{props.text}</p>
+            </div>
+            <div className="pro-btns">
+                < a className="btn" target="_blank" href={props.view}>View</a>
+                < a className="btn" target="_blank" href={props.source}>Source</a>
+            </div>
         </div>
-    </div>
-</div>
-  );
+    );
 };
 
 
